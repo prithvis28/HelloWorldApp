d6f1c6ed172eb7bba9958391a92d8c9f007615d8
prithvis28 <ps5252@srmist.edu.in>
2026-03-26 15:13:23 +0530

    UC6: Display Hello with Multiple Command-Line Arguments using substring

--- helloapp/HelloApp.java
+++ helloapp/HelloApp.java
@@ -5,9 +5,12 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
+            String names = "";
             for (String name : args) {
-                System.out.println("Hello, " + name + "!");
+                names += name + ", ";
             }
+            names = names.substring(0, names.length() - 2);
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
