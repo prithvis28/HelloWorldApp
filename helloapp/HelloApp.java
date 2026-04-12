455188b89a28c7cb797217bde8a1cc16f118972f
prithvis28 <ps5252@srmist.edu.in>
2026-04-12 23:20:33 +0530

    UC7: Display Hello with Multiple Command-Line Arguments using String.join

--- helloapp/HelloApp.java
+++ helloapp/HelloApp.java
@@ -5,11 +5,7 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String names = "";
-            for (String name : args) {
-                names += name + ", ";
-            }
-            names = names.substring(0, names.length() - 2);
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }
